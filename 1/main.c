25cb42e8dde95edbba1b95987d8a6ab87e4e472d
Eilza <liza050896kb@gmail.com>
2014-09-25 01:19:39 +0400

    Вынесла проверку корректности ввода в отдельную функцию. Назвала переменные не заглавными буквами.

--- 1/main.c
+++ 1/main.c
@@ -1,78 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// function for validating input
+int controlvvoda ( int Case )
+{
+    float coef = 0;
+
+    int number_was_inputed = 0;
+
+        while ( 1 )
+    {
+        if ( Case == 1 ) printf ( "\nInput a coefficient\n" );
+
+        else if ( Case == 2 ) printf ( "\nInput b coefficient\n" );
+
+        else printf ( "\ninput c coefficient\n" );
+
+        number_was_inputed = scanf ( "%f", &coef );
+
+        if ( number_was_inputed == 0 )
+           {
+               fflush ( stdin );
+               //Otherwise scanf does not work more than once
+
+               continue;
+           }
+
+        else
+               break;
+    }
+    return ( coef );
+}
+
 int main ()
 {
-    float A = 0.0, B =0.0, C =0.0, cor, cor1, cor2, discr, otv1, otv2;
+    float a, b, c, cor, cor1, cor2, discr, otv1, otv2;
 
-    // Проверим корректность ввода коэффициентов
-    int a_was_inputed = 0;
+    a = controlvvoda (1);
 
-    while ( 1 )
-    {
-        printf ( "\nInput A coefficient\n" );
+    b = controlvvoda (2);
 
-        fflush ( stdin );
-        // Иначе не работает scanf больше одного раза.
+    c = controlvvoda (3);
 
-        a_was_inputed = scanf ( "%f", &A );
 
-        if ( a_was_inputed == 0 )
-            continue;
+    discr = b*b - 4*a*c;
 
-        else
-            break;
-    }
-
-    int b_was_inputed = 0;
-
-    while ( 1 )
-    {
-        printf ( "\nInput B coefficient\n" );
-
-        fflush ( stdin );
-        // Иначе не работает scanf больше одного раза.
-
-        b_was_inputed = scanf ( "%f", &B );
-
-        if ( b_was_inputed == 0 )
-            continue;
-
-        else
-            break;
-    }
-
-    int c_was_inputed = 0;
-
-    while ( 1 )
-    {
-        printf ( "\nInput C coefficient\n" );
-
-        fflush ( stdin );
-        // Иначе не работает scanf больше одного раза.
-
-        c_was_inputed = scanf ( "%f", &C );
-
-        if ( c_was_inputed == 0 )
-            continue;
-
-        else
-            break;
-    }
-    discr = B*B - 4*A*C;
-
-    // Смотрим, чтоб дискриминант не был меньше нуля
+    // Look to the discriminant was not less than zero
     if ( discr < 0 )
         printf( "There are not any answers" );
 
     else
     {
-        otv1 = sqrt ( discr ) / ( 2*A );
-        otv2 = -B / ( 2*A );
+        otv1 = sqrt ( discr ) / ( 2*a );
+        otv2 = -b / ( 2*a );
 
-        if ( A == 0 )
+        if ( a == 0 )
         {
-            cor = -B / C;
+            cor = -c / b;
 
             printf( "\t%g", cor);
         }
@@ -81,7 +65,7 @@ int main ()
         {
             cor1 = otv1 + otv2;
             cor2 = otv1 - otv2;
-    // Проверяем, не совпадают ли корни
+    // check do not match if the roots
             if ( cor1 = cor2 )
                 printf ( "\tAnswers are: %g", cor1);
             else
