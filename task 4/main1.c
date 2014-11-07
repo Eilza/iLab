17e0e895f528886e0950c089ad1a24e861fa128e
Eilza <liza050896kb@gmail.com>
2014-11-07 03:36:02 +0300

    done

--- task 4/main1.c
+++ task 4/main1.c
@@ -1,77 +1,144 @@
-#include "stack.h"
 
-/*int main()
+
+int assembler ()
 {
+    FILE* source = fopen ("Source.txt", "r");
 
-    struct List* stack = NULL;
+    assert ( source != 0 );
 
-    if ( isEmpty( stack ) )
+    FILE* code = fopen ("Code.exe", "wb")
 
-        printf ( "stack is empty \n" );
-    else
-        printf ( "stack is not empty \n" );
+    char str[15], valuestr[15];
 
-    //fill stack
-    stack = Push ( stack, 54 );
-    stack = Push ( stack, 60 );
-
-    //see function push works correctly
-    if ( isEmpty( stack ) )
-
-        printf ("Push works incorrectly, stack is still empty \n");
-    else
-        printf ("Push works correctly \n ");
-
-     printf ("%d", Pop(&stack ) );
-
-     printf ("%d", getCount( stack ) );
-
-     stack = Delete ( stack );
-
-     return 0;
-}*/
-
-int conversion ()
-{
-    FILE* source = fopen ( "source.txt", "r");
-    assert ( source != 0);
-    FILE* code = code ( "code.txt", "w")
-    int c=0;
-    while ( (( c = getc( source )) != EOF) || (( c = getc( source )) != '\n') ) //there I don't know how select a piece of string until a space and compare it with the command
+    while (!feof(sourse))
     {
+        fscanf (sourse, "%s", str);
 
+        if (strcmp(str,"push") == 0 )
+        {
+            fprintf (code, "%d ", 555);
+
+            fscanf (sourse, "%s", valuestr);
+
+            fprintf (code, "%s\n",valuestr);
+
+            continue;
+        }
+        else if (strcmp(str,"pop") == 0)
+        {
+            fprintf (code, "%d ", 556);
+
+            continue;
+        }
+        else if (strcmp(str,"add") == 0)
+        {
+            fprintf (code, "%d ", 557);
+
+            continue;
+        }
+        else if (strcmp(str,"mul") == 0)
+        {
+            fprintf (code, "%d ", 558);
+
+            continue;
+        }
+        else if (strcmp(str,"jmp") == 0)
+        {
+            fprintf (code, "%d ", 559);
+
+            continue;
+        }
+        else if (strcmp(str,"end") == 0)
+        {
+            fprintf (code, "%d\n", 560);
+
+            fclose (sourse);
+
+            fclose (code);
+
+            return 1;
+        }
+        else if ( (strcmp(str, "\n") == 0 || strcmp(cmd, "") == 0) )
+        {
+            return -1;
+        }
+        else
+        {
+             fprintf (code, "%s", "\0");
+
+             fclose (sourse);
+
+             fclose (code);
+        }
     }
+
+int disassembler()
+{
+    FILE* code = fopen("Code.exe","rb");
+
+    FILE* newsourse = fopen("NewSourse.txt","w");
+
+    int command;
+
+    char valuestr[15];
+
+    while(!feof(code))
+    {
+        fscanf ( code, "%d", &command );
+
+        switch (command)
+        {
+            case 555:
+
+                fprintf (newsourse, "%s ", push);
+
+                fscanf (code, "%s", valuestr);
+
+                fprintf (newsourse, "%s\n", valuestr);
+
+                break;
+            case 556 :
+
+                fprintf (newsourse, "%s\n", pop);
+
+                break;
+            case 557:
+
+                fprintf (newsourse, "%s\n", add);
+
+                break;
+            case 558:
+
+                fprintf (newsourse, "%s ", mul);
+
+                fscanf (code, "%s", valuestr);
+
+                fprintf (newsourse, "%s\n", valuestr);
+
+                break;
+            case 559:
+
+                fprintf (newsourse, "%s\n", jmp);
+
+                break;
+            case 560:
+                fprintf (newsourse, "%s\n", push);
+
+                fclose (newsourse);
+
+                fclose (code);
+
+                return 0;
+
+                break;
+        }
+    }
+
+    fclose (newsourse);
+
+    fclose (code);
 }
 
-double get_code_by_file (char* str)
-{
-    if (strcmp(str,"push") == 0)
-    {
-        ;
-    }
-    else if (strcmp(str,"pop") == 0)
-    {
-        ;
-    }
-    else if (strcmp(str,"add") == 0)
-    {
-       ;
-    }
-    else if (strcmp(str,"mul") == 0)
-    {
-        ;
-    }
-    else if (strcmp(str,"end") == 0)
-    {
-        ;
-    }
-    else if (strcmp(str,"jmp") == 0)
-    {
-        ;
-    }
-    else if ( (strcmp(str, "\n") == 0 || strcmp(cmd, "") == 0) )
-    {
-        ;
-    }
 
-}
+
+
