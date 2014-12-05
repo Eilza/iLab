ae560d5f4ceccda518570fa76648c83cab6a650b
Eilza <liza050896kb@gmail.com>
2014-12-05 20:00:23 +0300

    added cmp,jmp,label,push_AX new files

--- task 4/assembler1.c
+++ task 4/assembler1.c
@@ -3,25 +3,80 @@
 #include <string.h>
 #include <assert.h>
 
-enum {PUSH=555,POP=556,ADD=557,MUL=558,JMP=559,END=560};
+enum {PUSH=555,POP=556,ADD=557,MUL=558,JMP=559,CMP=560,PUSH_AX=561,PUSH_BX=562,PUSH_CX=563,PUSH_DX=564,LABEL=565,END=566};
 
 int main ()
 {
+    int i=0,k=0;
+
     FILE* sourse = fopen ("Sourse.txt", "r");
 
     assert ( sourse != 0 );
 
     FILE* code = fopen ("Code1.txt", "wb");
 
-    char str[15], valuestr[15];
+    char str[25], valuestr[25];
 
     while (!feof(sourse))
     {
+        printf ("%s","tak");
+
         fscanf (sourse, "%s", str);
 
         if (strcmp(str,"push") == 0 )
         {
+            i++;
+
             fprintf (code, "%d ", PUSH);
+
+            fscanf (sourse, "%s", valuestr);
+
+            fprintf (code, "%s\t",valuestr);
+
+            continue;
+        }
+
+        if (strcmp(str,"push_AX") == 0 )
+        {
+            i++;
+
+            fprintf (code, "%d ", PUSH_AX);
+
+            fscanf (sourse, "%s", valuestr);
+
+            fprintf (code, "%s\t",valuestr);
+
+            continue;
+        }
+        if (strcmp(str,"push_BX") == 0 )
+        {
+            i++;
+
+            fprintf (code, "%d ", PUSH_BX);
+
+            fscanf (sourse, "%s", valuestr);
+
+            fprintf (code, "%s\t",valuestr);
+
+            continue;
+        }
+        if (strcmp(str,"push_CX") == 0 )
+        {
+            i++;
+
+            fprintf (code, "%d ", PUSH_CX);
+
+            fscanf (sourse, "%s", valuestr);
+
+            fprintf (code, "%s\t",valuestr);
+
+            continue;
+        }
+        if (strcmp(str,"push_DX") == 0 )
+        {
+            i++;
+
+            fprintf (code, "%d ", PUSH_DX);
 
             fscanf (sourse, "%s", valuestr);
 
@@ -31,25 +86,61 @@ int main ()
         }
         else if (strcmp(str,"pop") == 0)
         {
+            i++;
+
             fprintf (code, "%d\t", POP);
 
             continue;
         }
         else if (strcmp(str,"add") == 0)
         {
+            i++;
+
             fprintf (code, "%d\t", ADD);
 
             continue;
         }
         else if (strcmp(str,"mul") == 0)
         {
+            i++;
+
             fprintf (code, "%d\t", MUL);
 
             continue;
         }
         else if (strcmp(str,"jmp") == 0)
         {
+            i++;
+
             fprintf (code, "%d ", JMP);
+
+            fscanf (sourse, "%s", valuestr);
+
+            printf ("%s","sebe");
+
+            fprintf (code, "%s\t",LABEL);
+
+            fscanf (sourse, "%s", valuestr);
+
+            fprintf (code, "%s\t",valuestr);
+
+            continue;
+        }
+        else if (strcmp(str,"cmp") == 0)
+        {
+            i++;
+
+            fprintf (code, "%d\t", CMP);
+
+            continue;
+        }
+        if (strncmp(str,"label",5) == 0 )
+        {
+            i++;
+
+            k = i;
+
+            fprintf (code, "%d ", LABEL);
 
             fscanf (sourse, "%s", valuestr);
 
@@ -73,7 +164,7 @@ int main ()
         }
         else
         {
-             fprintf (code, "%s", "\0");
+             fprintf (code,"\0");
 
              fclose (sourse);
 
--- task 4/disassembler.c
+++ task 4/disassembler.c
@@ -44,10 +44,68 @@ int main ()
                 break;
             case 559:
 
-                fprintf (newsourse, "%s\n", "jmp");
+                fprintf (newsourse, "%s", "jmp");
+
+                fscanf (code, "%s", valuestr);
+
+                fprintf (newsourse, "%s ", valuestr);
+
+                fscanf (code, "%s", valuestr);
+
+                fprintf (newsourse, "%s\n", valuestr);
 
                 break;
             case 560:
+
+                fprintf (newsourse, "%s ", "cmp");
+
+                break;
+            case 561:
+
+                fprintf (newsourse, "%s ", "push_AX");
+
+                fscanf (code, "%s", valuestr);
+
+                fprintf (newsourse, "%s\n", valuestr);
+
+                break;
+            case 562:
+
+                fprintf (newsourse, "%s ", "push_BX");
+
+                fscanf (code, "%s", valuestr);
+
+                fprintf (newsourse, "%s\n", valuestr);
+
+                break;
+            case 563:
+
+                fprintf (newsourse, "%s ", "push_CX");
+
+                fscanf (code, "%s", valuestr);
+
+                fprintf (newsourse, "%s\n", valuestr);
+
+                break;
+            case 564:
+
+                fprintf (newsourse, "%s ", "push_DX");
+
+                fscanf (code, "%s", valuestr);
+
+                fprintf (newsourse, "%s\n", valuestr);
+
+                break;
+            case 565:
+
+                fprintf (newsourse, "%s ", "label");
+
+                fscanf (code, "%s", valuestr);
+
+                fprintf (newsourse, "%s\n", valuestr);
+
+                break;
+            case 566:
                 fprintf (newsourse, "%s\n", "end");
 
                 fclose (newsourse);
